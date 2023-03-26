e6bc38ebbfdcc1eb84085c2f20bd74f5b8694239
ALEX208z <anshumanayush208@gmail.com>
2023-03-26 13:20:15 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -10,10 +10,12 @@ import {
 // eslint-disable-next-line
 import { Article, Brand, CTA, Feature, Navbar } from "./components";
 import './App.css'
+// import { Routes, Route } from "react-router-dom"
 
 const App = () => {
   return (
     <div className="App">
+      
       <div className="gradient__bg">
         <Navbar />
         <Header />
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
-import  ReactDOM  from "react-dom";
+import ReactDOM from "react-dom";
+// import { HashRouter } from "react-router-dom";
 
-import App from './App'
-import './index.css'
+import App from "./App";
+import "./index.css";
 
-ReactDOM.render(<App/>,document.getElementById('root'))
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
